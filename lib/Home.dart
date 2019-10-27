1af6b5ac900ce87dc3c7b9ad2e36454cbef25da9
chansen424 <chansen424@yahoo.com>
2019-10-27 19:16:09 -0400

    Cannot return to login screen once logged in
    We don't want a driver to press the back button on their phone or in the app to return to the login page. If they are ever to return to the login page, it should only be as a consequence of logging out.

--- lib/Home.dart
+++ lib/Home.dart
@@ -6,18 +6,26 @@ import 'Map.dart';
 class Home extends StatelessWidget {
   BorderRadiusGeometry radius = BorderRadius.only(
       topLeft: Radius.circular(24.0), topRight: Radius.circular(24.0));
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Carriage'),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Carriage'),
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: (){},
+          ),
+        ),
+        body: SlidingUpPanel(
+            minHeight: 62.0,
+            panelSnapping: false,
+            panel: Upcoming(),
+            body: Map(),
+            borderRadius: radius),
       ),
-      body: SlidingUpPanel(
-          minHeight: 62.0,
-          panelSnapping: false,
-          panel: Upcoming(),
-          body: Map(),
-          borderRadius: radius),
     );
   }
 }
