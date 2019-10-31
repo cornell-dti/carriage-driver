899de4d857a65adc413bad4a10c6c1e852c4364a
chansen424 <chansen424@yahoo.com>
2019-10-30 22:57:31 -0400

    Formatting
    Removed slide_up_panel dependency also.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'Login.dart';
 
@@ -8,15 +7,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Carriage',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: Login()
-    );
+        title: 'Carriage',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: Login());
   }
 }
