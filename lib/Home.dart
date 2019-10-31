9d97919376d89799e6938cc8d8cbc1e569613827
chansen424 <chansen424@yahoo.com>
2019-10-30 22:54:29 -0400

    Navbar Highlight Selected

--- lib/Home.dart
+++ lib/Home.dart
@@ -5,16 +5,13 @@ import 'Map.dart';
 class Greeting extends StatelessWidget {
   String name;
 
-  Greeting({
-   @required this.name
-});
+  Greeting({@required this.name});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       child: Text('Hi $name!',
-          style:
-          TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
       padding: EdgeInsets.only(left: 30.0, top: 50.0, bottom: 10.0),
     );
   }
@@ -23,28 +20,40 @@ class Greeting extends StatelessWidget {
 class LeftSubheading extends StatelessWidget {
   String heading;
 
-  LeftSubheading({
-    @required this.heading
-  });
+  LeftSubheading({@required this.heading});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(left: 30.0, bottom: 15.0),
       child: Text('$heading',
-          style:
-          TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
     );
   }
 }
 
-Widget _ride(BuildContext context, int index) {
-  return NextRide();
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
 }
 
-class Home extends StatelessWidget {
+class _HomeState extends State<Home> {
   BorderRadiusGeometry radius = BorderRadius.only(
       topLeft: Radius.circular(24.0), topRight: Radius.circular(24.0));
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _ride(BuildContext context, int index) {
+    return NextRide();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,32 +69,29 @@ class Home extends StatelessWidget {
               SizedBox(height: 20.0),
               LeftSubheading(heading: 'Today\'s schedule'),
               Flexible(
-                child: Center(
-                  child: ListView.separated(
-                    itemCount: 3,
-                    itemBuilder: _ride,
-                    separatorBuilder: (BuildContext context, int index) => Divider(),
-                    padding: EdgeInsets.only(left: 30.0, right: 30.0, bottom: 5.0),
-                  ),
-                )
-              )
+                  child: Center(
+                child: ListView.separated(
+                  itemCount: 3,
+                  itemBuilder: _ride,
+                  separatorBuilder: (BuildContext context, int index) =>
+                      Divider(),
+                  padding:
+                      EdgeInsets.only(left: 30.0, right: 30.0, bottom: 5.0),
+                ),
+              ))
             ]),
-        bottomNavigationBar:  BottomNavigationBar(
+        bottomNavigationBar: BottomNavigationBar(
           selectedItemColor: Colors.blue,
           items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-              icon: Icon(Icons.star),
-              title: Text('Rides')
-            ),
+                icon: Icon(Icons.star), title: Text('Rides')),
             BottomNavigationBarItem(
-              icon: Icon(Icons.history),
-              title: Text('History')
-            ),
+                icon: Icon(Icons.history), title: Text('History')),
             BottomNavigationBarItem(
-                icon: Icon(Icons.account_circle),
-                title: Text('Profile')
-            )
+                icon: Icon(Icons.account_circle), title: Text('Profile'))
           ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
         ),
       ),
     );
