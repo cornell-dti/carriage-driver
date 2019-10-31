4d4d9c4b272318508fdde6cdf7b0e4b8e53c4388
chansen424 <chansen424@yahoo.com>
2019-10-30 22:27:59 -0400

    Homepage New Design
    Headings, Next Ride, Bottom Navigation Bar

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,32 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'Upcoming.dart';
 import 'Map.dart';
 
-class Location extends StatelessWidget {
-  Location({this.heading, this.destination});
+class Greeting extends StatelessWidget {
+  String name;
 
-  final String heading;
-  final String destination;
+  Greeting({
+   @required this.name
+});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('$heading', style: TextStyle(
-              fontSize: 20,
-              color: Colors.grey
-            )),
-            Text('$destination', style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold
-            ))
-          ],
-        ),
-      ],
+    return Padding(
+      child: Text('Hi $name!',
+          style:
+          TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+      padding: EdgeInsets.only(left: 30.0, top: 50.0, bottom: 10.0),
+    );
+  }
+}
+
+class LeftSubheading extends StatelessWidget {
+  String heading;
+
+  LeftSubheading({
+    @required this.heading
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 30.0, bottom: 15.0),
+      child: Text('$heading',
+          style:
+          TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
     );
   }
 }
@@ -40,104 +47,32 @@ class Home extends StatelessWidget {
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
-        body: SlidingUpPanel(
-            minHeight: 62.0,
-            panelSnapping: false,
-            panel: Upcoming(),
-            body: Scaffold(
-                body: Column(
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    child: Text('Hi Nick', style: TextStyle(fontSize: 40)),
-                    padding:
-                        EdgeInsets.only(left: 30.0, top: 50.0, bottom: 10.0),
-                  ),
-                ),
-                Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 30.0, bottom: 10.0),
-                      child: Text('Upcoming Rides',
-                          style: TextStyle(fontSize: 20.0)),
-                    )),
-                Center(
-                  child: Container(
-                    width: 350.0,
-                    height: 200.0,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(20.0),
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.grey,
-                              offset: Offset(5.0, 10.0),
-                              blurRadius: 5.0,
-                              spreadRadius: 0.0)
-                        ],
-                      ),
-                      child: Container(
-                        child: Row(
-                          children: <Widget>[
-                            Expanded(
-                              flex: 2,
-                              child: Column(
-                                children: <Widget>[
-                                  Padding(
-                                    padding:
-                                        EdgeInsets.only(left: 20.0, top: 20.0),
-                                    child: Align(
-                                      alignment: Alignment.centerLeft,
-                                      child: Text('Oct 13',
-                                          style: TextStyle(
-                                              fontSize: 20,
-                                              color: Colors.grey)),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding:
-                                        EdgeInsets.only(left: 20.0, top: 20.0),
-                                    child: Align(
-                                      alignment: Alignment.centerLeft,
-                                      child: Text('1:00 PM',
-                                          style: TextStyle(
-                                              fontSize: 30,
-                                              fontWeight: FontWeight.bold)),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Expanded(
-                              flex: 2,
-                              child: Column(
-                                children: <Widget>[
-                                  Padding(
-                                    padding: EdgeInsets.only(top: 20.0),
-                                    child: Column(
-                                        children: <Widget>[
-                                          Location(heading: 'From', destination: 'PSB'),
-                                          SizedBox(height: 16.0),
-                                          Location(heading: 'To', destination: 'Cascadilla')
-                                        ],
-                                    ),
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            )),
-            borderRadius: radius),
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Greeting(name: 'Chris'),
+              LeftSubheading(heading: 'Upcoming Rides'),
+              Center(child: NextRide()),
+              SizedBox(height: 20.0),
+              LeftSubheading(heading: 'Today\'s schedule')
+            ]),
+        bottomNavigationBar:  BottomNavigationBar(
+          selectedItemColor: Colors.blue,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              title: Text('Rides')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history),
+              title: Text('History')
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle),
+                title: Text('Profile')
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/Upcoming.dart
+++ lib/Upcoming.dart
@@ -1,147 +1,151 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
-List<String> upcoming = [
-  "Chris @ 12pm",
-  "Laura @ 3pm",
-  "Sam @ 4:30pm",
-  "David @ 5pm",
-  "Swasti @ 7am tomorrow",
-  "Paulie @ 8am tomorrow",
-  "Lisa @ 9am tomorrow"
-];
-int eta = 13;
+class CircleImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double _size = 50.0;
 
-class _CurrentRideState extends State<CurrentRide> {
-  List<String> cur = [
-    "Sabriyah",
-    "11am",
-    "Cascadilla",
-    "PSB",
-    "+1 123 456 7890"
-  ];
-  bool _inProgress = false;
-  bool _messaging = false;
+    return Container(
+      width: _size,
+      height: _size,
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+              fit: BoxFit.cover, image: AssetImage('assets/images/terry.jpg'))),
+    );
+  }
+}
 
+class Rider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: _messaging ? 200.0 : 150.0,
-        child: Padding(
-            padding: EdgeInsets.only(left: 16.0),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Text('${cur[0]} ${_inProgress ? '' : '@ ${cur[1]}'}',
-                      style: Theme.of(context).textTheme.headline),
-                  Text(_inProgress ? 'ETA $eta minutes' : 'From ${cur[2]}',
-                      style: Theme.of(context).textTheme.headline),
-                  Text('To ${cur[3]}',
-                      style: Theme.of(context).textTheme.headline),
-                  Row(
-                    children: <Widget>[
-                      RaisedButton(
-                        child: Text(_inProgress ? 'Here' : 'Arrived'),
-                        color: Colors.white,
-                        onPressed: () {
-                          setState(() {
-                            _inProgress = true;
-                          });
-                        },
-                      ),
-                      SizedBox(width: 10.0),
-                      RaisedButton(
-                        child: Icon(Icons.call, color: Colors.green),
-                        color: Colors.white,
-                        onPressed: () {
-                          launch("tel:${cur[4]}");
-                        },
-                      ),
-                      SizedBox(width: 10.0),
-                      RaisedButton(
-                        child: Icon(Icons.message, color: Colors.black),
-                        color: Colors.white,
-                        onPressed: () {
-                          setState(() {
-                            _messaging = !_messaging;
-                          });
-                        },
-                      )
-                    ],
-                  ),
-                  Visibility(
-                    visible: _messaging,
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
+      child: Row(
+        children: <Widget>[
+          CircleImage(),
+          SizedBox(width: 10.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Terry Cruz', style: TextStyle(fontWeight: FontWeight.bold)),
+              Text('Big Arms', style: TextStyle(color: Colors.grey))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Location extends StatelessWidget {
+  Location({this.heading, this.destination});
+
+  final String heading;
+  final String destination;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('$heading',
+                style: TextStyle(fontSize: 14, color: Colors.grey)),
+            Text('$destination',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class Date extends StatelessWidget {
+  String date;
+
+  Date({@required this.date});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 20.0, top: 20.0),
+      child: Text('$date', style: TextStyle(fontSize: 20, color: Colors.grey)),
+    );
+  }
+}
+
+class Time extends StatelessWidget {
+  String time;
+
+  Time({@required this.time});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 20.0, top: 5.0),
+      child: Text('$time',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+    );
+  }
+}
+
+class NextRide extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 350.0,
+      height: 170.0,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey,
+                offset: Offset(5.0, 10.0),
+                blurRadius: 5.0,
+                spreadRadius: 0.0)
+          ],
+        ),
+        child: Container(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 4,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Date(date: 'Oct 30'),
+                    Time(time: '10:12 PM'),
+                    Padding(
+                        padding: const EdgeInsets.only(left: 20.0, top: 10.0),
+                        child: Rider())
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: Column(
                         children: <Widget>[
-                          ActionChip(
-                            label: Text('Where are you?'),
-                            onPressed: () {
-                              launch('sms:${cur[4]}?body=Where are you%3F');
-                            },
-                          ),
-                          SizedBox(width: 5.0),
-                          ActionChip(
-                            label: Text('Let me pull around.'),
-                            onPressed: () {
-                              launch('sms:${cur[4]}?body=Let me pull around.');
-                            },
-                          ),
-                          SizedBox(width: 5.0),
-                          ActionChip(
-                            label: Text('Be there soon!'),
-                            onPressed: (){
-                              launch('sms:${cur[4]}?body=Be there soon%21');
-                            },
-                          ),
-                          SizedBox(width: 10.0)
+                          Location(heading: 'From', destination: 'PSB'),
+                          SizedBox(height: 16.0),
+                          Location(heading: 'To', destination: 'Cascadilla')
                         ],
                       ),
-                    ),
-                  )
-                ])));
-  }
-}
-
-class CurrentRide extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _CurrentRideState();
-  }
-}
-
-class Schedule extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: upcoming.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('${upcoming[index]}'),
-          );
-        });
-  }
-}
-
-class Upcoming extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          SizedBox(height: 10.0),
-          Align(
-            alignment: Alignment.center,
-            child: SizedBox(
-                height: 5.0, width: 70.0, child: Container(color: Colors.grey)),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
-          SizedBox(height: 10.0),
-          Text('Your Rides', style: Theme.of(context).textTheme.headline),
-          CurrentRide(),
-          Flexible(child: Schedule())
-        ],
+        ),
       ),
     );
   }
