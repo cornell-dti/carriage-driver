8ac28ed3dd1e926b5cef0f78787dc40ec01a2d2d
Bryan Tabor <bjt84@cornell.edu>
2020-11-10 18:28:36 -0500

    unnecessary using

--- lib/Rides.dart
+++ lib/Rides.dart
@@ -1,6 +1,4 @@
-import 'dart:convert';
 import 'package:carriage/MeasureRect.dart';
-import 'package:carriage/app_config.dart';
 import 'package:carriage/RidesProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
