8b5647d56fb530de670bfd8d17d4a556a9f2551e
Tony Yang <txy3@cornell.edu>
2021-05-10 02:08:41 -0400

    finish push notifications

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -6,6 +6,15 @@ import 'Rides.dart';
 import 'Profile.dart';
 import '../providers/DriverProvider.dart';
 import '../utils/CarriageTheme.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import '../utils/app_config.dart';
+import '../providers/AuthProvider.dart';
+import '../utils/NotificationService.dart';
 
 class Greeting extends StatelessWidget {
   @override
@@ -51,11 +60,174 @@ class _HomeState extends State<Home> {
 
   int _selectedIndex = RIDES;
 
+  //Initialize notification variables
+  static final FirebaseMessaging _fcm = FirebaseMessaging();
+  static FlutterLocalNotificationsPlugin notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  StreamSubscription iosSubscription; // ignore: cancel_subscriptions
+  String deviceToken;
+
   @override
   void initState() {
     super.initState();
-
     LocationTracker.initialize();
+
+    final AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+    final initializationSettingsIOS = IOSInitializationSettings();
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
+    );
+    notificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
+    initialize();
+    getMessage();
+  }
+
+  Future<void> onSelectNotification(String payload) {
+    Navigator.push(
+        context, new MaterialPageRoute(builder: (context) => Home()));
+    return Future<void>.value();
+  }
+
+  _registerOnFirebase() async {
+    //_fcm.subscribeToTopic('all');
+    await _fcm.getToken().then((token) => deviceToken = token);
+
+    if (deviceToken != null) {
+      subscribe(deviceToken);
+    }
+    _fcm.onTokenRefresh.listen((newToken) {
+      subscribe(newToken);
+    });
+  }
+
+  void initialize() async {
+    _registerOnFirebase();
+    if (Platform.isIOS) {
+      iosSubscription = _fcm.onIosSettingsRegistered.listen((data) {
+        // save the token  OR subscribe to a topic here
+        _fcm.subscribeToTopic('all');
+      });
+      await _fcm.requestNotificationPermissions(
+          IosNotificationSettings(sound: true, badge: true, alert: true));
+    }
+  }
+
+  // Show notification banner on background and foreground.
+  static void showNotification(String notification) async {
+    final AndroidNotificationDetails androidPlatformChannelSpecifics =
+        await getAndroidNotificationDetails(notification);
+    final IOSNotificationDetails iOSPlatformChannelSpecifics =
+        IOSNotificationDetails();
+    final NotificationDetails platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iOSPlatformChannelSpecifics);
+
+    await notificationsPlugin.show(
+      0,
+      'Carriage Driver',
+      notification,
+      platformChannelSpecifics,
+    );
+  }
+
+  static Future<AndroidNotificationDetails> getAndroidNotificationDetails(
+      dynamic notification) async {
+    return AndroidNotificationDetails('general', 'General notifications',
+        'General notifications that are not sorted to any specific topics.',
+        importance: Importance.max,
+        priority: Priority.high,
+        showWhen: false,
+        category: 'General',
+        icon: '@mipmap/ic_launcher',
+        largeIcon: DrawableResourceAndroidBitmap('@mipmap/ic_launcher'));
+  }
+
+  void getMessage() async {
+    PushNotificationMessageAndroid androidNotification;
+    PushNotificationMessageIOS iosNotification;
+
+    _fcm.configure(
+      onBackgroundMessage: Platform.isIOS ? null : backgroundHandle,
+      onMessage: (Map<String, dynamic> message) async {
+        // print("onMessage: $message");
+        if (Platform.isAndroid) {
+          androidNotification =
+              PushNotificationMessageAndroid.fromJson(message);
+        } else {
+          iosNotification = PushNotificationMessageIOS.fromJson(message);
+        }
+        Platform.isIOS
+            ? showNotification(iosNotification.body)
+            : showNotification(androidNotification.body);
+        setState(() {});
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        //print("onLaunch: $message");
+        if (Platform.isAndroid) {
+          androidNotification =
+              PushNotificationMessageAndroid.fromJson(message);
+        } else {
+          iosNotification = PushNotificationMessageIOS.fromJson(message);
+        }
+        Navigator.push(
+            context, new MaterialPageRoute(builder: (context) => Home()));
+        setState(() {});
+      },
+      onResume: (Map<String, dynamic> message) async {
+        //print("onResume: $message");
+        if (Platform.isAndroid) {
+          androidNotification =
+              PushNotificationMessageAndroid.fromJson(message);
+        } else {
+          iosNotification = PushNotificationMessageIOS.fromJson(message);
+        }
+        Navigator.push(
+            context, new MaterialPageRoute(builder: (context) => Home()));
+        setState(() {});
+      },
+    );
+  }
+
+  static Future<dynamic> backgroundHandle(
+    Map<String, dynamic> message,
+  ) async {
+    if (message.containsKey('data')) {
+      // Handle data message
+      final dynamic data = message['data']['default'];
+      showNotification('$data');
+      print("_backgroundMessageHandler data: $data");
+    }
+    if (message.containsKey('notification')) {
+      // Handle notification message
+      final dynamic notification = message['notification']['body'];
+      showNotification('$notification');
+      print("_backgroundMessageHandler notification: $notification");
+    }
+    return Future<void>.value();
+  }
+
+  subscribe(String token) async {
+    AuthProvider authProvider =
+        Provider.of<AuthProvider>(context, listen: false);
+    String authToken = await authProvider.secureStorage.read(key: 'token');
+    final response = await http.post(
+      "${AppConfig.of(context).baseUrl}/notification/subscribe",
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        HttpHeaders.authorizationHeader: "Bearer $authToken"
+      },
+      body: jsonEncode(<String, String>{
+        'platform': 'android',
+        'token': token,
+      }),
+    );
+    if (response.statusCode != 200) {
+      throw Exception('Failed to subscribe.');
+    }
   }
 
   void _onItemTapped(int index) {
@@ -63,6 +235,7 @@ class _HomeState extends State<Home> {
       _selectedIndex = index;
     });
   }
+
   Widget getPage(BuildContext context, int index) {
     switch (index) {
       case (RIDES):
@@ -87,11 +260,26 @@ class _HomeState extends State<Home> {
           unselectedItemColor: Colors.grey,
           items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-              icon: Image.asset(_selectedIndex == 0 ? 'assets/images/carIconBlack.png' : 'assets/images/carIconGrey.png', width: 20), label: 'Rides'),
+                icon: Image.asset(
+                    _selectedIndex == 0
+                        ? 'assets/images/carIconBlack.png'
+                        : 'assets/images/carIconGrey.png',
+                    width: 20),
+                label: 'Rides'),
             BottomNavigationBarItem(
-                icon: Image.asset(_selectedIndex == 1 ? 'assets/images/clockIconBlack.png' : 'assets/images/clockIconGrey.png', width: 20), label: 'History'),
+                icon: Image.asset(
+                    _selectedIndex == 1
+                        ? 'assets/images/clockIconBlack.png'
+                        : 'assets/images/clockIconGrey.png',
+                    width: 20),
+                label: 'History'),
             BottomNavigationBarItem(
-                icon: Image.asset(_selectedIndex == 2 ? 'assets/images/profileIconBlack.png' : 'assets/images/profileIconGrey.png', width: 20), label: 'Profile')
+                icon: Image.asset(
+                    _selectedIndex == 2
+                        ? 'assets/images/profileIconBlack.png'
+                        : 'assets/images/profileIconGrey.png',
+                    width: 20),
+                label: 'Profile')
           ],
           currentIndex: _selectedIndex,
           onTap: _onItemTapped,
