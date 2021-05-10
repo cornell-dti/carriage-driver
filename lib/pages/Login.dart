3624639bb8c7dc97b6aea4c451da76a032e0fa39
helenxuyang <helen.xu.yang@gmail.com>
2021-05-10 04:06:50 -0400

    Updated login page (copied from rider)

--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,10 +1,12 @@
+import 'package:carriage/providers/AuthProvider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/AuthProvider.dart';
 
 class Login extends StatelessWidget {
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(context) {
     AuthProvider authProvider = Provider.of(context);
     try {
       authProvider.signInSilently();
@@ -13,53 +15,97 @@ class Login extends StatelessWidget {
           'User has not logged in previously, therefore, we should not proceed');
     }
     return Scaffold(
-        body: Container(
-            color: Colors.white,
-            child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    FlutterLogo(size: 150),
-                    SizedBox(height: 50),
-                    SignInButton()
-                  ],
-                )
+      backgroundColor: Colors.black,
+      body: Container(
+        margin: EdgeInsets.only(bottom: 20),
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+//        mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 6,
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text('Welcome to Carriage',
+                        style: TextStyle(fontSize: 33, color: Colors.white)),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.only(left: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text('Sign in using your Cornell email',
+                      style: TextStyle(fontSize: 15, color: Colors.white54)),
+                ],
+              ),
+            ),
+            SizedBox(height: 60.0),
+            Image.asset(
+              'assets/images/app_logo.png',
+              height: 270,
+              width: 270,
+            ),
+            Expanded(
+              child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: SignInButton(),
+              ),
             )
-        )
+          ],
+        ),
+      ),
     );
   }
 }
 
 class SignInButton extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(context) {
     AuthProvider authProvider = Provider.of(context);
-    return OutlineButton(
+    return ButtonTheme(
+      minWidth: MediaQuery.of(context).size.width * 0.8,
+      child: FlatButton(
+        color: Colors.white,
         splashColor: Colors.grey,
-        onPressed: authProvider.signIn,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-        highlightElevation: 0,
-        borderSide: BorderSide(color: Colors.grey),
+        onPressed: () {
+          authProvider.signIn();
+        },
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3)),
         child: Padding(
-            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image(
-                    image: AssetImage('assets/images/google_logo.png'),
-                    height: 35.0),
-                Padding(
-                  padding: EdgeInsets.only(left: 10),
-                  child: Text(
-                    'Sign in with Google',
-                    style: TextStyle(fontSize: 20, color: Colors.grey),
-                  ),
-                )
-              ],
-            )
-        )
+          padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                  image: AssetImage('assets/images/google_logo.png'),
+                  height: 20.0),
+              SizedBox(
+                width: 8,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Text(
+                  'Sign in with Google',
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
