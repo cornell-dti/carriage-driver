cf443c414bee425ef3be48153a2001673026a350
Tony Yang <txy3@cornell.edu>
2021-05-10 01:45:43 -0400

    setup android files for notifs

--- android/app/src/main/kotlin/com/example/carriage/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/carriage/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.carriage
 
-import io.flutter.embedding.android.FlutterActivity;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
