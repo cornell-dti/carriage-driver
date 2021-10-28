177d785e946927a679aa2137da1f5f5785268987
Matthew Guo <mguo22@gmail.com>
2021-10-27 21:53:35 -0400

    change cornelldti to carriage

--- android/app/src/main/kotlin/com/carriage/carriage_driver/Application.kt
+++ android/app/src/main/kotlin/com/carriage/carriage_driver/Application.kt
@@ -1,4 +1,4 @@
-package com.cornelldti.carriage_driver
+package com.carriage.carriage_driver
 
 import io.flutter.app.FlutterApplication
 import io.flutter.plugin.common.PluginRegistry
--- android/app/src/main/kotlin/com/carriage/carriage_driver/MainActivity.kt
+++ android/app/src/main/kotlin/com/carriage/carriage_driver/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.cornelldti.carriage_driver
+package com.carriage.carriage_driver
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
